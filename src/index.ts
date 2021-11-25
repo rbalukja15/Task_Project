77e362c18c8845355e05411de567529612c4d744
Romarjo Balukja <mbalukja1@gmail.com>
2021-11-25 15:53:13 +0100

    Implement put method.

--- src/index.ts
+++ src/index.ts
@@ -11,14 +11,14 @@ app.use(bodyParser.urlencoded({limit: "200mb", extended: true}));
 
 createConnection().then(async connection => {
 
-    app.get('/', async (req, res) => {
+    app.get('/users', async (req, res) => {
         const repository = getRepository(User);
         const users = await repository.find();
         res.send({ users })
         console.log(users)
     });
 
-    app.post('/', async (req, res) => {
+    app.post('/users', async (req, res) => {
         console.log(req.body)
         const repository = getRepository(User);
         await repository.insert({
@@ -28,16 +28,34 @@ createConnection().then(async connection => {
         res.status(200).send({ message: 'Success'});
     });
 
-    app.patch('/', (req,res) => {
+    app.patch('/users/:userId', (req,res) => {
         res.send({ route: 'home', data: 'This is patch req' })
     })
 
-    app.put('/', (req, res) => {
-        console.log(req.body)
-        // res.send({ route: 'home', data: 'this is put req' })
+    app.put('/users/:userId', async (req, res) => {
+        const userId = +req.params.userId;
+
+        try {
+            if (!userId) {
+                return res.send({ status: 400, message: 'userId is not provided'});
+            } else {
+                const repository = getRepository(User);
+                const user = await repository.findOne({ id: userId });
+                if (!user) {
+                    return res.send({ status: 404, message: `User with id ${userId} not found`});
+                } else {
+                    const savedRes = await repository.merge(user, req.body);
+                    await repository.save(savedRes);
+                    return res.send({ status: 200, message: `User with id ${userId} saved successfully`});
+                }
+            }
+        } catch (error) {
+            console.log(error)
+            return res.send({ status: 500, message: `Server error`});
+        }
     })
 
-    app.delete('/', (req, res) => {
+    app.delete('/users', (req, res) => {
         res.send({ route: 'home', data: 'This is delete req' })
     })
 
